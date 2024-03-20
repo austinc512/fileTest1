c3cc660f31fb46408bce276619decece45e60b47
austinc512 <cello.austin1994@gmail.com>
2024-03-19 19:11:11 -0500

    added error handling

--- index.js
+++ index.js
@@ -21,26 +21,30 @@ const upload = multer({ storage: storage });
 
 // Route to upload a file
 app.post('/upload', upload.single('file'), (req, res) => {
-  const file = req.file;
-  if (!file) {
-    const body = req.body;
-    console.log({ body });
-    return res.status(200).json({ message: 'MADE IT TO THE END!!!' });
+  try {
+    const file = req.file;
+    if (!file) {
+      const body = req.body;
+      console.log({ body });
+      return res.status(200).json({ message: 'MADE IT TO THE END!!!' });
+    }
+    const headers = req.headers;
+    //   console.log(req.body);
+    console.log({ headers });
+
+    // Here you can use fs to manipulate the file, but multer already saved it
+    console.log(`Received and saved file: ${file.originalname}`);
+
+    // contrive an id property that will be returned from the wordpress API response
+    const fakeId = Math.trunc(Math.random() * 1000) + 1;
+
+    return res.json({
+      message: `File uploaded successfully: ${file.originalname}`,
+      id: fakeId,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-  const headers = req.headers;
-  //   console.log(req.body);
-  console.log({ headers });
-
-  // Here you can use fs to manipulate the file, but multer already saved it
-  console.log(`Received and saved file: ${file.originalname}`);
-
-  // contrive an id property that will be returned from the wordpress API response
-  const fakeId = Math.trunc(Math.random() * 1000) + 1;
-
-  res.json({
-    message: `File uploaded successfully: ${file.originalname}`,
-    id: fakeId,
-  });
 });
 
 // Start the server
